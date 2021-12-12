c33e370c726cc58e1e2fac0d92f0e69feebba63f
Suman Chandra Shil <sshil@pinterest.com>
2021-12-11 18:33:06 -0800

    Create notes in an infinite loop

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         [],
     )?;
 
-    let mut buffer =  String::new();
-    io::stdin().read_line(&mut buffer)?;
-    conn.execute("INSERT INTO notes (body) values (?1)", [buffer])?;
+    let mut running = true;
+    while running == true {
+        let mut buffer =  String::new();
+        io::stdin().read_line(&mut buffer)?;
+        let trimmed_body = buffer.trim();
+        if trimmed_body == "" {
+            running = false;
+        } else {
+            conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body])?;
+        }
+    }
     Ok(())
 }
