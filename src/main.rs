a3fad2d5715dec90d4b7da3e66019f95d4d860fb
Suman Chandra Shil <sshil@pinterest.com>
2021-12-12 13:00:32 -0800

    Implement edit and delete functionality

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer)?;
         let trimmed_body = buffer.trim();
+        let cmd_split = trimmed_body.split_once(" ");
+
+        let mut cmd = trimmed_body;
+        let mut msg = "";
+
+        if cmd_split != None {
+            cmd = cmd_split.unwrap().0;
+            msg = cmd_split.unwrap().1;
+        }
         if trimmed_body == "" {
             running = false;
+        } else if cmd == "/del" {
+            let id = msg;
+            conn.execute("delete from notes where id = (?1)", [id])?;
+        } else if cmd == "/edit" {
+            let msg_split = msg.split_once(" ").unwrap();
+            let id = msg_split.0;
+            let body = msg_split.1;
+            conn.execute("update notes set body = (?1) where id = (?2)", [body, id])?;
         } else if  trimmed_body == "/list" {
             let mut stmt = conn.prepare("SELECT id, body from notes")?;
             let mut rows = stmt.query(rusqlite::params![])?;
@@ -24,7 +41,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let body: String = row.get(1)?;
                 println!("{} {}", id, body.to_string());
             }
-
         } else {
             conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body])?;
         }
