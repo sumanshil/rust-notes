d52f4cd7d6dec73d2cf632c726707cefc0987726
Suman Chandra Shil <sshil@pinterest.com>
2021-12-12 12:48:49 -0800

    Select all the notes from DB

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io;
 use rusqlite::{Connection, Result};
-
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conn = Connection::open("notes.db")?;
     conn.execute(
         "create table if not exists notes (
@@ -10,14 +9,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     )",
         [],
     )?;
-
     let mut running = true;
     while running == true {
-        let mut buffer =  String::new();
+        let mut buffer = String::new();
         io::stdin().read_line(&mut buffer)?;
         let trimmed_body = buffer.trim();
         if trimmed_body == "" {
             running = false;
+        } else if  trimmed_body == "/list" {
+            let mut stmt = conn.prepare("SELECT id, body from notes")?;
+            let mut rows = stmt.query(rusqlite::params![])?;
+            while let Some(row) = rows.next()? {
+                let id: i32 = row.get(0)?;
+                let body: String = row.get(1)?;
+                println!("{} {}", id, body.to_string());
+            }
+
         } else {
             conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body])?;
         }
